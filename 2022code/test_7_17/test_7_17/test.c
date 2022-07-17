bd6593106aee41715de524ce39e75a79466f87a9
bolinx2003 <3154756364@qq.com>
2022-07-17 22:13:50 +0800

    提交代码

---
+++ 2022code/test_7_17/test_7_17/test.c
@@ -0,0 +1,170 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("%d\n", 10);
+//	//printf("%d\n", 20);
+//
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	short age = 10;
+//	int num = 100;
+//	float weight = 55.5f;
+//	char ch = 'w';
+//
+//	return 0;
+//}
+
+
+//int b = 20;
+//
+//void test()
+//{
+//	int c = 1000;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//
+//	return 0;
+//}
+
+
+//int a = 10;
+//
+//void test()
+//{
+//	printf("test:%d\n", a);
+//}
+//
+//int main()
+//{
+//	printf("main:%d\n", a);
+//	test();
+//
+//	return 0;
+//}
+
+
+//int a = 100;
+//
+//int main()
+//{
+//	int a = 10;
+//	printf("a=%d\n", a);
+//
+//	return 0;
+//}
+
+
+//写一个代码求2个整数的和
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//
+//	//输入
+//	scanf("%d%d", &num1, &num2);
+//	//求和
+//	sum = num1 + num2;
+//	//输出
+//	printf("sum = %d\n", sum);
+//
+//	//printf("%d %d\n", num1, num2);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[5] = { 0 };
+//	scanf("%s", arr);
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
+
+
+//int a = 10;
+//
+//void test()
+//{
+//	printf("test: %d\n", a);
+//}
+//
+//int main()
+//{
+//	test();
+//	printf("main: %d\n", a);
+//
+//	return 0;
+//}
+
+
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n", g_val);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	{
+//		int a = 10;
+//		printf("%d\n", a);
+//	}
+//	printf("%d\n", a);//err
+//
+//	return 0;
+//}
+
+
+//int b = 0;
+//
+//int main()
+//{
+//
+//	return 0;
+//}
+
+//int a = 10;
+//
+//void test()
+//{
+//	printf("a=%d\n", a);
+//}
+//
+//int main()
+//{
+//	test();
+//
+//	return 0;
+//}
