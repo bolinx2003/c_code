8fd91a9c697864fd83d950cfa183904e8752244b
bolinx2003 <3154756364@qq.com>
2022-07-19 21:14:02 +0800

    提交代码

---
+++ 2022code/test_7_19/test_7_19/test.c
@@ -0,0 +1,364 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+
+//
+//void test()
+//{
+//	int b = 100;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	return 0;
+//}
+
+
+
+//int num = 10;
+//
+//int main()
+//{
+//	int num = 1;
+//	printf("num = %d\n", num);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("     **\n");
+//	printf("     **\n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *\n");
+//	printf("    *  *\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d", &x);
+//
+//	if (x < 0)
+//		y = 1;
+//	else if (x == 0)
+//		y = 0;
+//	else
+//		y = -1;
+//
+//	printf("%d\n", y);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int x = 0;
+//	scanf("%d", &x);
+//
+//	if (x < 0)
+//		printf("%d\n", 1);
+//	else if (x == 0)
+//		printf("%d\n", 0);
+//	else
+//		printf("%d\n", -1);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//
+//	if (m % 5 == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\121"));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[] = { 'b', 'i', 't' };
+//	printf("%d\n", strlen(arr));
+//
+//	return 0;
+//}
+
+
+//int a;
+//
+//int main()
+//{
+//	int b;
+//	printf("%d\n", b);
+//	//printf("%d\n", a);
+//
+//	return 0;
+//}
+
+//
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//
+//	scanf("%d %d", &n1, &n2);
+//	int m = Max(n1, n2);
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+//
+
+
+//int main()
+//{
+//	int iq = 0;
+//	scanf("%d", &iq);
+//
+//	if (iq >= 140)
+//		printf("Genius\n");
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int iq = 0;
+//	//while (scanf("%d", &iq) != EOF)
+//	while (scanf("%d", &iq) == 1)
+//	{
+//		if (iq >= 140)
+//			printf("Genius\n");
+//	}
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	scanf("%d %d", &a, &b);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     man\n");
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d\n", a / b, a % b);
+//
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//
+//	int r = (-8 + 22) * a - 10 + c / 2;
+//	printf("%d\n", r);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	//int a = 0;
+//	//int b = !a;
+//
+//	//printf("b=%d\n", b);
+//
+//	int flag = 5;
+//	
+//	if (flag)
+//		printf("hehe\n");
+//
+//	if (!flag)
+//		printf("haha\n");
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	//int a = -10;
+//	//int b = -a;
+//	//int c = +10;
+//	//c = +a;
+//
+//	//int a = 10;
+//
+//	//printf("%d\n", sizeof(a));
+//	//printf("%d\n", sizeof(int));
+//
+//	//int a = 10;
+//	////int b = ++a;
+//	//int b = a++;
+//	//printf("a=%d b=%d\n", a, b);
+//
+//	//int a = 10;
+//	////int b = --a;
+//	//int b = a--;
+//	//printf("a=%d b=%d\n", a, b);
+//
+//	//int a = 1;
+//	//int b = (++a) + (++a) + (++a);
+//	//printf("%d\n", b);
+//
+//	int a = (int)3.14;
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	if (a == 3 && b == 5)
+//	{
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int m = 0;
+//
+//	if (a > b)
+//		m = a;
+//	else
+//		m = b;
+//
+//	m = ((a > b) ? a : b);
+//
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	int a = 1;
+//	int b = 3;
+//	int c = 4;
+//	int d = (a = b - 3, b = a * 2, c = a - b);
+//
+//	printf("%d %d %d %d\n", a, b, c ,d);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5 };
+//	printf("%d\n", arr[4]);
+//
+//	return 0;
+//}
+
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+//int main()
+//{
+//	printf("hello");
+//	int m = Max(2, 3);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	auto int a = 10;
+//	//int char;//err
+//	return 0;
+//}
+
+//
+//typedef unsigned int uint;
+//
+//int main()
+//{
+//	unsigned int num1 = 0;
+//	uint num2 = 0;
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	register int num = 10;
+//
+//	return 0;
+//}
+//
+//
+
+
